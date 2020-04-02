f19a47134e98bc88a82989fe1ed516b83881df54
LittleBlue418 <hollyathomas88@gmail.com>
2020-04-02 13:45:56 +0200

    state change on button click

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Holly', age: 32, hobbies: 'Writing' },
+      { name: 'Martin', age: 41, hobbies: 'Programming' },
+      { name: 'Morris', age: 11, hobbies: 'Sleeping' },
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Holly Thomas', age: 32, hobbies: 'Writing' },
+        { name: 'Martin', age: 41, hobbies: 'Programming' },
+        { name: 'Morris', age: 11, hobbies: 'Sleeping' },
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>The Fam</h1>
-        <Person name="Holly" age="32">My Hobbies: Writing</Person>
-        <Person name="Martin" age="41">My Hobbies: Programming</Person>
-        <Person name="Morris" age="11">My Hobbies: Sleeping</Person>
+        <button onClick={this.switchNameHandler}>Switch Member</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Writing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Programming</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Sleeping</Person>
       </div>
       //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a React App!!!'))
     );
