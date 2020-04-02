feb6f1061ea99583b26e7491da5bd5cfb691614d
LittleBlue418 <hollyathomas88@gmail.com>
2020-04-02 12:02:58 +0200

    Props and adding information

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App!</h1>
-        <p>We love JavaSript</p>
-        <Person />
-        <Person />
-        <Person />
+        <h1>The Fam</h1>
+        <Person name="Holly" age="32">My Hobbies: Writing</Person>
+        <Person name="Martin" age="41">My Hobbies: Programming</Person>
+        <Person name="Morris" age="11">My Hobbies: Sleeping</Person>
       </div>
       //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a React App!!!'))
     );
