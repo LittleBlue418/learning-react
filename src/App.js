efe680e29c805abb3b2a5f142d990fb7f1d33c43
LittleBlue418 <hollyathomas88@gmail.com>
2020-04-04 11:01:53 +0200

    stylesheet & inline styling

--- src/App.js
+++ src/App.js
@@ -33,10 +33,20 @@ class App extends Component {
   }
 
   render () {
+    const buttonStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>The Fam</h1>
-        <button onClick={() => this.switchNameHandler('LittleBlue')}>Show Last Names</button>
+        <button
+          style={buttonStyle}
+          onClick={() => this.switchNameHandler('LittleBlue')}>Show Last Names</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
